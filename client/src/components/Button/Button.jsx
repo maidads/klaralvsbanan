5f614a5f40c74e9fcf0c7382748d9bec06bd5455
Loushymooshy <ellika.larsson2@gmail.com>
2025-02-25 07:01:16 +0100

    Comment: button instruction

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./Button.css"
 
+// När du har skapat en komponent som heter Button och lagt till den i App.jsx, så skickar du med props med text och icon. Om icon finns, så visas den.
+//Exempel på hur komponenten skrivs: <Button text="Click Me" icon={myIcon} />
+
 const Button = ({ text, icon}) => {
   return (
     <> 
