390d96139a2d6edcc8ad86452be3d5ce0cb618be
Rolf Sparv <hrothi.wulfaz@gmail.com>
2025-02-27 23:34:32 +0100

    style: styled the trail path

--- client/src/components/MapView/MapView.jsx
+++ client/src/components/MapView/MapView.jsx
@@ -1,10 +1,40 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup, GeoJSON } from 'react-leaflet';
+import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect, useRef, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap, GeoJSON } from 'react-leaflet';
+import { filterOutPlaces } from '../../utils/apis/turid';
 import "./MapView.css";
-import { useEffect, useState } from 'react';
-import { filterOutPlaces, isWithinRange } from '../../utils/apis/turid';
+import ZoomControls from "./ZoomControls";
 import geoJson from "../../assets/geojson/banan.geojson?raw";
 const trackPoints = JSON.parse(geoJson);
+
+function MapView() {
+
+    const mapRef = useRef();
+
+    useEffect(() => {
+        if (!mapRef.current) {
+            mapRef.current = leaflet.map('map').setView([59.37843, 13.50846], 13);
+
+            leaflet
+            .tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(mapRef.current);
+        }
+
+        return () => {
+            if (mapRef.current) {
+                mapRef.current.remove();
+                mapRef.current = null;
+            }
+        };
+    }, []);
+
+    return (
+        <div id="map" style={{ height: '100vh' }}></div>
+    );
+}
 
 const ChangeView = ({ position }) => {
     const map = useMap();
@@ -14,20 +44,13 @@ const ChangeView = ({ position }) => {
     return null
 }
 
-const MapView = ({ turidData }) => {
+const MapViewReact = ({ turidData }) => {
     const [position, setPosition] = useState([59.3793, 13.5036]);
-    const [mapPosition, setMapPosition] = useState([59.3793, 13.5036]);
-    const [lastPosition, setLastPosition] = useState(null);
 
     useEffect(() => {
         const positionTracker = navigator.geolocation.watchPosition(
             location => {
-                const currentPosition = [location.coords.latitude, location.coords.longitude];
-                setPosition(currentPosition);
-                if (!isWithinRange(lastPosition, currentPosition, 50)) {
-                    setMapPosition(currentPosition);
-                    setLastPosition(currentPosition);
-                }
+                setPosition([location.coords.latitude, location.coords.longitude]);
             }, error => {
                 console.error("Error fetching user location", error);
             }, {
@@ -59,28 +82,41 @@ const MapView = ({ turidData }) => {
     }
 
     return (
-        <div style={{position: "relative", height: "100vh", width: "100vw"}}>
-            <MapContainer center={mapPosition} zoom={13} style={{ height: '100%', width: '100%' }}>
-            <ChangeView position={mapPosition} />
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-            />
-            {trackPoints && <GeoJSON data={trackPoints} style={whiteStyle}/>}
-            {trackPoints && <GeoJSON data={trackPoints} style={pathStyle}/>}
-            {places && places.map(place => (
-                place.latitude && place.longitude ? (
-                    <Marker key={place.id} position={[place.latitude, place.longitude]}>
-                        <Popup>{place.title}</Popup>
-                    </Marker>
-                ) : null
-            ))}
-            <Marker position={position}>
-                <Popup>Du</Popup>
-            </Marker>
+        <div className="map-container" style={{ position: "relative", height: "100vh", width: "100vw" }}>
+            <MapContainer 
+                center={position} 
+                zoom={13} 
+                zoomControl={false}  // 🔹 Tar bort Leaflets standardzoom
+                style={{ height: "100%", width: "100%" }}
+            >
+                <ChangeView position={position} />
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+                />
+
+                {trackPoints && <GeoJSON data={trackPoints} style={whiteStyle}/>}
+                {trackPoints && <GeoJSON data={trackPoints} style={pathStyle}/>}
+                
+                {/* 🔹 Rendera alla platser från Turid API */}
+                {places && places.map(place => (
+                    place.latitude && place.longitude ? (
+                        <Marker key={place.id} position={[place.latitude, place.longitude]}>
+                            <Popup>{place.title}</Popup>
+                        </Marker>
+                    ) : null
+                ))}
+    
+                {/* 🔹 Markör för användarens position */}
+                <Marker position={position}>
+                    <Popup>Du</Popup>
+                </Marker>
+    
+                {/* 🔹 Anpassade zoomknappar */}
+                <ZoomControls />
             </MapContainer>
         </div>
     );
 };
 
-export default MapView;
+export default MapViewReact;
